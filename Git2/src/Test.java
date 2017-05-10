74d748a7bc2f0ba860cb02724fec1ad1f4859863
Kai Schäfer <r4w@me.com>
2017-05-10 15:30:10 +0200

    3rd

--- Git2/src/Test.java
+++ Git2/src/Test.java
@@ -3,4 +3,15 @@
 public class Test {
 
     private String name;
+
+
+    public String getName () {
+	return name;
+    }
+
+
+    public void setName (String name) {
+	this.name = name;
+    }
+
 }
